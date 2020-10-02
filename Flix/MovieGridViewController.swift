17f1f36d90a0fba8a2cfa9d9d5d28fa1499666b1
spryby <sergeyprybysh@gmail.com>
2020-10-01 22:15:57 -0700

    Style superhero GridLayout

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -21,6 +21,12 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         collectionView.delegate = self
         collectionView.dataSource = self
 
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height:  width * 3 / 2)
+
         fetchData()
     }
     
